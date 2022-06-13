ed64fed8dd3073810a226dc7e5e8926b404df67a
spiessenst <spiessenst@gmail.com>
2022-06-13 23:15:51 +0200

    storeupdate

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -84,7 +84,7 @@ class ListController extends AbstractController
             }
 
             foreach ($lists as $list) {
-                $lists_array[] = ['shopppinglist_id' => $list->getShoppinglistId(),
+                $lists_array[] = ['shoppinglist_id' => $list->getShoppinglistId(),
                     'shoppinglist_create_date' => $list->getShoppinglistCreateDateString(),
                     'shoppinglist_name' => $list->getShoppinglistName()];
             }
